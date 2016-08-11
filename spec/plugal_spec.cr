5eb0c5083ab5909fd7494cff16f6cc896586183b
hyronx <hyronx@outlook.com>
2016-08-11 14:39:17 +0200

    Creating specs

--- spec/plugal_spec.cr
+++ spec/plugal_spec.cr
@@ -3,6 +3,24 @@ require "./spec_helper"
 describe Plugal do
   # TODO: Write tests
 
+  Plugal.command :test, first: String, second: Int32, result: String
+  
+  class TestProvider
+    include Plugal::Provider
+  
+    provide :test do |first, second|
+      "#{first} : #{second}"
+    end
+  end
+  
+  class TestReceiver
+    include Plugal::Receiver
+  
+    receive :test do |result|
+      puts result.data
+    end
+  end
+
   it "works" do
     false.should eq(true)
   end
