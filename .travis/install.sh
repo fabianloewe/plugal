cf1ba826125ae8dadfc7f4f35cac07d5dc77accf
Fabian Loewe <hyronx@outlook.com>
2016-08-17 11:09:18 +0200

    Travis Tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,8 @@ if [[ $TRAVIS_OS_NAME == "osx" ]]; then
   brew update
   brew install redis
 else
+  sudo apt-get update
+  sudo apt-get install gcc
   wget http://download.redis.io/redis-stable.tar.gz
   tar xvzf redis-stable.tar.gz
   cd redis-stable
